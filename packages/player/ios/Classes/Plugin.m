069f681eeaa2bd9520fd462ba22a15ddd90f9ffd
Alan Lira <alanmlira@gmail.com>
2020-02-06 15:34:31 -0300

    revert issue with play n pause events

--- packages/player/ios/Classes/Plugin.m
+++ packages/player/ios/Classes/Plugin.m
@@ -79,6 +79,8 @@ dispatch_queue_t serialQueue = nil;
       [commandCenter.playCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
           if (_playerId != nil) {
               [self resume:_playerId];
+              int state = STATE_PLAYING;
+              [_channel_player invokeMethod:@"state.change" arguments:@{@"playerId": _playerId, @"state": @(state)}];
           }
           return MPRemoteCommandHandlerStatusSuccess;
       }];
@@ -86,6 +88,8 @@ dispatch_queue_t serialQueue = nil;
       [commandCenter.pauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
           if (_playerId != nil) {
               [self pause:_playerId];
+              int state = STATE_PAUSED;
+              [_channel_player invokeMethod:@"state.change" arguments:@{@"playerId": _playerId, @"state": @(state)}];
           }
           return MPRemoteCommandHandlerStatusSuccess;
       }];
