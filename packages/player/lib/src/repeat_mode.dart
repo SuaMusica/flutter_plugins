c5dcca11b52c43df05120ba5e3436aff102089bd
Alan Trope <atrope@gmail.com>
2020-02-17 12:39:19 -0300

    adding description to Enum

--- packages/player/lib/src/repeat_mode.dart
+++ packages/player/lib/src/repeat_mode.dart
@@ -1,5 +1,16 @@
-enum RepeatMode {
-  NONE,
-  QUEUE,
-  TRACK
+enum RepeatMode { NONE, QUEUE, TRACK }
+
+extension ParseToString on RepeatMode {
+  String toShortString() {
+    switch (this) {
+      case RepeatMode.NONE:
+        return 'Disabled';
+      case RepeatMode.QUEUE:
+        return 'Queue';
+      case RepeatMode.TRACK:
+        return 'Track';
+      default:
+        return "Unknown";
+    }
+  }
 }
