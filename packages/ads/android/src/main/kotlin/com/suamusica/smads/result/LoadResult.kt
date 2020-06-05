edf937c9edf0f784afb2c40030e6e004f234bc75
jefersonkrauss <jeferson.krauss@gmail.com>
2020-06-04 21:57:45 -0300

    [Feature/785] - Mapping results for plugin methods.

---
+++ packages/ads/android/src/main/kotlin/com/suamusica/smads/result/LoadResult.kt
@@ -0,0 +1,10 @@
+package com.suamusica.smads.result
+
+class LoadResult {
+    companion object {
+        const val SUCCESS = 1
+        const val UNKNOWN_ERROR = 0
+        const val NO_CONNECTIVITY = -1
+        const val SCREEN_IS_LOCKED = -2
+    }
+}
---
+++ packages/ads/android/src/main/kotlin/com/suamusica/smads/result/ScreenStatusResult.kt
@@ -0,0 +1,8 @@
+package com.suamusica.smads.result
+
+class ScreenStatusResult {
+    companion object {
+        const val LOCKED_SCREEN = 0
+        const val UNLOCKED_SCREEN = 1
+    }
+}
