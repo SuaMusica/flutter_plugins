edeed9d7118d1835d9d94fec1d1ff9aed1d7a400
jefersonkrauss <jeferson.krauss@gmail.com>
2020-06-04 21:58:45 -0300

    [Feature/785] - Implementing wrapper for client callback.

---
+++ packages/ads/android/src/main/kotlin/com/suamusica/smads/SmadsCallback.kt
@@ -0,0 +1,26 @@
+package com.suamusica.smads
+
+import com.suamusica.smads.output.AdEventOutput
+import com.suamusica.smads.output.ErrorOutput
+import io.flutter.plugin.common.MethodChannel
+
+class SmadsCallback(private val channel: MethodChannel) {
+
+    fun onAddEvent(adEventOutput: AdEventOutput) {
+        channel.invokeMethod(ON_AD_EVENT_METHOD, adEventOutput.toResult())
+    }
+
+    fun onComplete() {
+        channel.invokeMethod(ON_COMPLETE_METHOD, mapOf<Any, Any>())
+    }
+
+    fun onError(error: ErrorOutput) {
+        channel.invokeMethod(ON_ERROR_METHOD, error.toResult())
+    }
+
+    companion object {
+        private const val ON_AD_EVENT_METHOD = "onAdEvent"
+        private const val ON_COMPLETE_METHOD = "onComplete"
+        private const val ON_ERROR_METHOD = "onError"
+    }
+}
