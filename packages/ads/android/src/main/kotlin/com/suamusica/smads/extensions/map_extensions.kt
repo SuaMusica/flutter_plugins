c12f702d5609f058c1481e9995156eb351477ea9
Jeferson Krauss <jeferson.krauss@gmail.com>
2020-06-09 19:10:38 -0300

    [Feature/785] - Mapping params for load method.

---
+++ packages/ads/android/src/main/kotlin/com/suamusica/smads/extensions/map_extensions.kt
@@ -0,0 +1,19 @@
+package com.suamusica.smads.extensions
+
+import java.lang.IllegalStateException
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Map<String, Any>.getRequired(key: String): T {
+    val value = this[key] ?: throw IllegalStateException("key $key can not be null")
+    return value as T
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Map<String, Any>.getValueOrNull(key: String): T? {
+    return this[key] as? T
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Map<String, Any>.getValueOrDefault(key: String, or: () -> T): T {
+    return (this[key] as? T) ?: or.invoke()
+}
--- packages/ads/android/src/main/kotlin/com/suamusica/smads/input/LoadMethodInput.kt
+++ packages/ads/android/src/main/kotlin/com/suamusica/smads/input/LoadMethodInput.kt
@@ -1,19 +1,31 @@
 package com.suamusica.smads.input
 
+import com.suamusica.smads.extensions.getRequired
+import com.suamusica.smads.extensions.getValueOrNull
+
 data class LoadMethodInput(
-        val url: String,
-        val contentUrl: String
+        val adTagUrl: String,
+        val contentUrl: String,
+        val age: Int?,
+        val gender: String?,
+        val typeAd: String
 ) {
-    private constructor(args: Map<String, String>)
+    private constructor(args: Map<String, Any>)
             : this(
-            args.getValue(URL_KEY),
-            args.getValue(CONTENT_URL_KEY))
+            args.getRequired(AD_TAG_URL_KEY),
+            args.getRequired(CONTENT_URL_KEY),
+            args.getValueOrNull(AGE_KEY),
+            args.getValueOrNull(GENDER_KEY),
+            args.getRequired(TYPE_AD_KEY))
 
     @Suppress("UNCHECKED_CAST")
-    constructor(args: Any) : this(args = args as Map<String, String>)
+    constructor(args: Any) : this(args = args as Map<String, Any>)
 
     companion object {
-        private const val URL_KEY = "__URL__"
+        private const val AD_TAG_URL_KEY = "__URL__"
         private const val CONTENT_URL_KEY = "__CONTENT__"
+        private const val AGE_KEY = "age"
+        private const val GENDER_KEY = "gender"
+        private const val TYPE_AD_KEY = "typead"
     }
 }
