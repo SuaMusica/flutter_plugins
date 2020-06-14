649907c462105a957ddbf89c547596d784267897
Jeferson Krauss <jeferson.krauss@gmail.com>
2020-06-14 13:13:24 -0300

    [Feature/785] - Implementing ScreenManager to verify Screen Status.

--- packages/ads/android/src/main/kotlin/com/suamusica/smads/ScreenManager.kt
+++ packages/ads/android/src/main/kotlin/com/suamusica/smads/ScreenManager.kt
@@ -1,8 +1,11 @@
 package com.suamusica.smads
 
+import android.app.KeyguardManager
 import android.content.Context
 
 object ScreenManager {
-    fun areInBackground(context: Context) = false
-    fun isLocked(context: Context) = false
+    fun isLocked(context: Context): Boolean {
+        val keyguardManager: KeyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        return keyguardManager.isKeyguardLocked
+    }
 }
