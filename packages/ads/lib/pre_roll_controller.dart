00394126fb2668f59314300332018ccddc7d1e3b
Jeferson Krauss <jeferson.krauss@gmail.com>
2020-07-07 23:40:17 -0300

    [Feature/786] - Adding function on constructor to confirm the play action.

--- packages/ads/lib/pre_roll_controller.dart
+++ packages/ads/lib/pre_roll_controller.dart
@@ -5,22 +5,33 @@ import 'package:smads/pre_roll_events.dart';
 class PreRollController extends PreRollEventHandler {
   final MethodChannel _channel;
 
-  PreRollController(Function(PreRollEvent, Map<String, dynamic>) listener)
+  PreRollController(Function(PreRollEvent, Map<String, dynamic>) listener,
+      Future<bool> Function() canIPlay)
       : _channel = MethodChannel('suamusica/pre_roll'),
+        _canIPlay = canIPlay ?? _iCanPlay(),
         super(listener) {
     if (listener != null) {
       _channel.setMethodCallHandler(handleEvent);
     }
   }
+
+  final Future<bool> Function() _canIPlay;
+
   Future<int> get screenStatus async =>
       await _channel.invokeMethod('screen_status');
+
+  static Future<bool> _iCanPlay() => Future.value(true);
 
   void pause() {
     _channel.invokeMethod("pause");
   }
 
   void play() {
-    _channel.invokeMethod("play");
+    _canIPlay.call().then((value) => {
+      if (value) {
+        _channel.invokeMethod("play")
+      }
+    });
   }
 
   void load(Map<String, dynamic> args) {
