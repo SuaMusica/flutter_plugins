94af331802e08e4cfa99d63839439cb502a2ebbf
jefersonkrauss <jeferson.krauss@gmail.com>
2020-08-22 18:12:10 -0300

    [Feature/433] - Initial commit for media scanner plugin.

---
+++ packages/mediascanner/android/src/main/kotlin/com/suamusica/mediascanner/MediaScannerPlugin.kt
@@ -0,0 +1,42 @@
+package com.suamusica.mediascanner
+
+import android.os.Build
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+/** MediaScannerPlugin */
+public class MediaScannerPlugin: FlutterPlugin, MethodCallHandler {
+
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "MediaScanner")
+    channel.setMethodCallHandler(this);
+  }
+
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "MediaScanner")
+      channel.setMethodCallHandler(MediaScannerPlugin())
+    }
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
---
+++ packages/mediascanner/ios/Classes/MediaScannerPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface MediaScannerPlugin : NSObject<FlutterPlugin>
+@end
