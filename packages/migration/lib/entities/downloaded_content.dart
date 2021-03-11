e5bdb871ea7e86cf355bc16354e07084e787d4a5
lstonussi <lucastonussi@gmail.com>
2021-03-11 15:10:35 -0300

    little adjusts

--- packages/migration/lib/entities/downloaded_content.dart
+++ packages/migration/lib/entities/downloaded_content.dart
@@ -1,15 +1,15 @@
 class DownloadedContent {
   DownloadedContent({
-    this.id,
-    this.path,
+    required this.id,
+    required this.path,
   });
-  final String? id;
-  final String? path;
+  final String id;
+  final String path;
 
   factory DownloadedContent.fromJson(Map<dynamic, dynamic> json) =>
       DownloadedContent(
-        id: json['id'] as String?,
-        path: json['path'] as String?,
+        id: json['id'] as String,
+        path: json['path'] as String,
       );
 
   Map<String, dynamic> toJson() => {
