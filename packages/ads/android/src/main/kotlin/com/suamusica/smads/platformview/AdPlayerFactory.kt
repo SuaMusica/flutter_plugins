f93a68de3df1a85cf42206e257ca1111065a9687
Alan Trope <atrope@gmail.com>
2022-03-25 13:00:50 -0300

    chore: upgrade player and ads to build

--- packages/ads/android/src/main/kotlin/com/suamusica/smads/platformview/AdPlayerFactory.kt
+++ packages/ads/android/src/main/kotlin/com/suamusica/smads/platformview/AdPlayerFactory.kt
@@ -8,8 +8,9 @@ import io.flutter.plugin.platform.PlatformViewFactory
 import timber.log.Timber
 
 class AdPlayerFactory(private val controller: AdPlayerViewController): PlatformViewFactory(StandardMessageCodec.INSTANCE) {
-  override fun create(context: Context, viewId: Int, args: Any): PlatformView {
+  override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
     Timber.d("create(viewId = %s, args=%s)", viewId, args)
     return AdPlayer(controller, AdPlayerParams(args))
   }
+
 }
