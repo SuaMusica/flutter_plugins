569322096d87166c311e14f7bc268f0ac50d1035
Felipe Sales <soufeliposales@gmail.com>
2024-04-15 16:54:22 -0300

    temp: temporarily removed android to not make the necessary migration now

--- packages/ads/android/src/main/kotlin/com/suamusica/smads/result/LoadResult.kt
+++
@@ -1,8 +0,0 @@
-package com.suamusica.smads.result
-
-class LoadResult {
-    companion object {
-        const val SUCCESS = 1
-        const val UNKNOWN_ERROR = 0
-    }
-}
--- packages/ads/android/src/main/kotlin/com/suamusica/smads/result/ScreenStatusResult.kt
+++
@@ -1,8 +0,0 @@
-package com.suamusica.smads.result
-
-class ScreenStatusResult {
-    companion object {
-        const val IS_BACKGROUND = 0
-        const val IS_FOREGROUND = 1
-    }
-}
