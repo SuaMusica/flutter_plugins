5f41c7ecf8f9d079847c81078f46e47a077fa809
lstonussi <lucastonusi@gmail.com>
2024-09-30 15:32:47 -0300

    new queue

--- packages/player/ios/Classes/SMPlayer.swift
+++ packages/player/ios/Classes/SMPlayer.swift
@@ -8,7 +8,10 @@ public class SMPlayer : NSObject  {
     var methodChannelManager: MethodChannelManager?
     private var smPlayer: AVQueuePlayer
     private var playerItem: AVPlayerItem?
-//    private var queue : [PlaylistItem] = []
+    public var currentIndex: Int = 0
+    private var history: [AVPlayerItem] = []
+//    private var currentQueue: [AVPlayerItem] = []
+    private var upcomingItems: [AVPlayerItem] = []
     
     
     init(methodChannelManager: MethodChannelManager?) {
@@ -52,8 +55,8 @@ public class SMPlayer : NSObject  {
                let playerItem = AVPlayerItem(asset: AVURLAsset(url: url, options: assetOptions))
             playerItem.playlistItem = media
     
-            smPlayer.insert(playerItem, after: smPlayer.items().isEmpty ? nil : smPlayer.items().last)
-            
+//            smPlayer.insert(playerItem, after: smPlayer.items().isEmpty ? nil : smPlayer.items().last)
+            addItem(playerItem)
             
             print("enqueued: \(smPlayer.items().count)")
         }
@@ -77,6 +80,51 @@ public class SMPlayer : NSObject  {
         smPlayer.advanceToNextItem()
     }
     
+    func nextTrack() {
+           if let currentItem = smPlayer.currentItem {
+               history.append(currentItem)
+           }
+        smPlayer.advanceToNextItem()
+        updateQueue(from:"nextTrack")
+        NowPlayingCenter.set(item: getCurrentPlaylistItem())
+       }
+       
+    func previousTrack() {
+        guard let lastHistoryItem = history.popLast() else { return }
+        let previousItem = recreatePlayerItem(from: lastHistoryItem)
+        if let currentItem = smPlayer.currentItem {
+            let newItem = recreatePlayerItem(from: currentItem)
+            upcomingItems.insert(newItem, at: 0)
+        }
+        
+        
+//        upcomingItems.insert(previousItem, at: 0)
+        smPlayer.replaceCurrentItem(with: previousItem)
+        updateQueue(from:"previousTrack")
+        NowPlayingCenter.set(item: getCurrentPlaylistItem())
+    }
+
+    private func updateQueue(from:String) {
+        print("------------------------------------------")
+           while !upcomingItems.isEmpty {
+               print("#printStatus upcomingItems: \(String(describing: upcomingItems[0].playlistItem?.title))")
+               let item = upcomingItems.removeFirst()
+                let newItem = recreatePlayerItem(from: item)
+               smPlayer.insert(newItem, after: from == "nextTrack" ? smPlayer.items().last : smPlayer.currentItem)
+               }
+               printStatus()
+       }
+    
+    private func recreatePlayerItem(from item: AVPlayerItem) -> AVPlayerItem {
+        guard let asset = item.asset as? AVURLAsset else {
+            fatalError("Unable to get asset from AVPlayerItem")
+        }
+        let newItem = AVPlayerItem(asset: asset)
+        newItem.playlistItem = item.playlistItem
+        return newItem
+    }
+
+    
     func removeAll(){
         smPlayer.removeAllItems()
 //        queue.removeAll()
@@ -87,6 +135,11 @@ public class SMPlayer : NSObject  {
     func play(){
         smPlayer.play()
     }
+    
+    func addItem(_ item: AVPlayerItem) {
+           upcomingItems.append(item)
+        updateQueue(from:"nextTrack")
+       }
     
     func seekToPosition(position:Int){
             
@@ -135,7 +188,24 @@ public class SMPlayer : NSObject  {
          }
      }
     
-    
+    func printStatus() {
+        print("#printStatus #################################################")
+            print("#printStatus History: \(history.count) items")
+            for item in history {
+                print("#printStatus History: \(String(describing: item.playlistItem?.title))")
+            }
+            print("#printStatus ------------------------------------------")
+            print("#printStatus Upcoming Items: \(upcomingItems.count) items")
+            for item in upcomingItems {
+                print("#printStatus Upcoming: \(String(describing: item.playlistItem?.title))")
+            }
+            print("#printStatus ------------------------------------------")
+            print("#printStatus AVQueuePlayer items: \(smPlayer.items().count)")
+            for item in smPlayer.items() {
+                print("#printStatus AVQueuePlayer: \(String(describing: item.playlistItem?.title))")
+            }
+        print("#printStatus #################################################")
+        }
     
    
 }
