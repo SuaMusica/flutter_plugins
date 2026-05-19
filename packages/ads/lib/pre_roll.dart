9dcd263045fe676bb9a834ad9fe2faaf0487aa66
Felipe Sales <soufeliposales@gmail.com>
2026-05-19 15:10:10 -0300

    Refactor PreRoll widget to use StatefulWidget for improved state management
    - Changed PreRoll from StatelessWidget to StatefulWidget to manage internal state more effectively.
    - Introduced a new _PreRollState class to handle platform view creation and lifecycle events.
    - Updated the build method to use SizedBox.shrink() for unsupported platforms and improved layout constraints.
    - Streamlined platform view creation logic for Android and iOS, enhancing code clarity and maintainability.

--- packages/ads/lib/pre_roll.dart
+++ packages/ads/lib/pre_roll.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +5,13 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:smads/pre_roll_controller.dart';
 
-class PreRoll extends StatelessWidget {
+class PreRoll extends StatefulWidget {
   final double? maxHeight;
   final PreRollController? controller;
-  final bool useHybridComposition, useinitExpensiveAndroidView;
-  PreRoll({
+  final bool useHybridComposition;
+  final bool useinitExpensiveAndroidView;
+
+  const PreRoll({
     Key? key,
     this.maxHeight,
     this.controller,
@@ -20,83 +20,88 @@ class PreRoll extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<PreRoll> createState() => _PreRollState();
+}
+
+class _PreRollState extends State<PreRoll> {
+  double? get maxHeight => widget.maxHeight;
+  PreRollController? get controller => widget.controller;
+  bool get useHybridComposition => widget.useHybridComposition;
+  bool get useinitExpensiveAndroidView => widget.useinitExpensiveAndroidView;
+
+  static const _viewType = 'suamusica/pre_roll_view';
+  static const _creationParams = <String, dynamic>{
+    'adSize': {'width': 640, 'height': 480},
+  };
+  static const _codec = StandardMessageCodec();
+  static const _gestureRecognizers = <Factory<OneSequenceGestureRecognizer>>{};
+
+  final UniqueKey _viewKey = UniqueKey();
+
+  void _onPlatformViewCreated(int id) {
+    if (defaultTargetPlatform == TargetPlatform.android) controller?.play();
+  }
+
+  @override
   Widget build(BuildContext context) {
     if (defaultTargetPlatform != TargetPlatform.android &&
         defaultTargetPlatform != TargetPlatform.iOS) {
-      return Container();
+      return const SizedBox.shrink();
     }
-    final UniqueKey _key = UniqueKey();
-    final viewType = 'suamusica/pre_roll_view';
-    final creationParams = <String, dynamic>{
-      'adSize': {
-        'width': 640,
-        'height': 480,
-      },
-    };
 
-    debugPrint('[PREROLL] Building PreRoll');
+    final maxHeightItem =
+        maxHeight ?? MediaQuery.maybeSizeOf(context)?.height ?? double.infinity;
 
-    return Container(
-      constraints: BoxConstraints(
-        maxHeight: maxHeight ?? MediaQuery.of(context).size.height,
+    return RepaintBoundary(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxHeight: maxHeightItem),
+        child: defaultTargetPlatform == TargetPlatform.android
+            ? (useHybridComposition
+                ? PlatformViewLink(
+                    key: _viewKey,
+                    viewType: _viewType,
+                    surfaceFactory: (_, controller) => AndroidViewSurface(
+                          controller: controller as AndroidViewController,
+                          gestureRecognizers: _gestureRecognizers,
+                          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+                        ),
+                    onCreatePlatformView: (params) =>
+                        (useinitExpensiveAndroidView
+                            ? PlatformViewsService.initExpensiveAndroidView(
+                                id: params.id,
+                                viewType: _viewType,
+                                layoutDirection: TextDirection.ltr,
+                                creationParams: _creationParams,
+                                creationParamsCodec: _codec,
+                              )
+                            : PlatformViewsService.initSurfaceAndroidView(
+                                id: params.id,
+                                viewType: _viewType,
+                                layoutDirection: TextDirection.ltr,
+                                creationParams: _creationParams,
+                                creationParamsCodec: _codec,
+                              ))
+                          ..addOnPlatformViewCreatedListener((id) {
+                            params.onPlatformViewCreated(id);
+                            _onPlatformViewCreated(id);
+                          })
+                          ..create())
+                : AndroidView(
+                    key: _viewKey,
+                    viewType: _viewType,
+                    creationParams: _creationParams,
+                    creationParamsCodec: _codec,
+                    onPlatformViewCreated: _onPlatformViewCreated,
+                    clipBehavior: Clip.none,
+                  ))
+            : UiKitView(
+                key: _viewKey,
+                viewType: _viewType,
+                creationParams: _creationParams,
+                creationParamsCodec: _codec,
+                onPlatformViewCreated: _onPlatformViewCreated,
+              ),
       ),
-      child: defaultTargetPlatform == TargetPlatform.android
-          ? (useHybridComposition
-              ? PlatformViewLink(
-                  key: _key,
-                  viewType: viewType,
-                  surfaceFactory: (BuildContext context,
-                          PlatformViewController controller) =>
-                      AndroidViewSurface(
-                    controller: controller as AndroidViewController,
-                    gestureRecognizers: const <Factory<
-                        OneSequenceGestureRecognizer>>{},
-                    hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-                  ),
-                  onCreatePlatformView: (PlatformViewCreationParams params) =>
-                      (useinitExpensiveAndroidView
-                          ? PlatformViewsService.initExpensiveAndroidView(
-                              id: params.id,
-                              viewType: viewType,
-                              layoutDirection: TextDirection.ltr,
-                              creationParams: creationParams,
-                              creationParamsCodec: const StandardMessageCodec(),
-                            )
-                          : PlatformViewsService.initSurfaceAndroidView(
-                              id: params.id,
-                              viewType: viewType,
-                              layoutDirection: TextDirection.ltr,
-                              creationParams: creationParams,
-                              creationParamsCodec: const StandardMessageCodec(),
-                            ))
-                        ..addOnPlatformViewCreatedListener((int id) {
-                          params.onPlatformViewCreated(id);
-                          _onPlatformViewCreated(id);
-                        })
-                        ..create(),
-                )
-              : AndroidView(
-                  key: _key,
-                  viewType: viewType,
-                  creationParams: creationParams,
-                  creationParamsCodec: const StandardMessageCodec(),
-                  onPlatformViewCreated: _onPlatformViewCreated,
-                  clipBehavior: Clip.none,
-                ))
-          : UiKitView(
-              key: _key,
-              viewType: viewType,
-              creationParams: creationParams,
-              creationParamsCodec: const StandardMessageCodec(),
-              onPlatformViewCreated: _onPlatformViewCreated,
-            ),
     );
-  }
-
-  void _onPlatformViewCreated(int id) {
-    debugPrint('Platform view created withs id: $id');
-    if (Platform.isAndroid) {
-      controller?.play();
-    }
   }
 }
