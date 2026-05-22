54cc56e404308916200e08542f222b02badd4f53
Alan Trope <atrope@gmail.com>
2026-05-22 01:06:36 -0300

    Refactor PreRoll widget to enhance platform-specific rendering and state management
    - Changed PreRoll from a StatelessWidget to a StatefulWidget to manage hybrid composition support on Android.
    - Introduced separate classes for Android and iOS rendering, improving code organization and readability.
    - Updated the build method to utilize a switch statement for platform-specific views, enhancing maintainability.
    - Added logic to check for hybrid composition support during initialization, ensuring compatibility with various Android configurations.

--- packages/ads/lib/pre_roll.dart
+++ packages/ads/lib/pre_roll.dart
@@ -7,96 +7,227 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:smads/pre_roll_controller.dart';
 
-class PreRoll extends StatelessWidget {
-  final double? maxHeight;
-  final PreRollController? controller;
-  final bool useHybridComposition, useinitExpensiveAndroidView;
-  PreRoll({
-    Key? key,
+const _kPreRollViewType = 'suamusica/pre_roll_view';
+const _kPreRollCreationParamsCodec = StandardMessageCodec();
+const _kPreRollCreationParams = <String, dynamic>{
+  'adSize': {
+    'width': 640,
+    'height': 480,
+  },
+};
+
+class PreRoll extends StatefulWidget {
+  const PreRoll({
+    super.key,
     this.maxHeight,
     this.controller,
     this.useHybridComposition = false,
     this.useinitExpensiveAndroidView = false,
-  }) : super(key: key);
+  });
+
+  final double? maxHeight;
+  final PreRollController? controller;
+  final bool useHybridComposition;
+  final bool useinitExpensiveAndroidView;
+
+  @override
+  State<PreRoll> createState() => _PreRollState();
+}
+
+class _PreRollState extends State<PreRoll> {
+  bool? _hcppSupported;
+  final Key _platformViewKey = UniqueKey();
+
+  @override
+  void initState() {
+    super.initState();
+    if (defaultTargetPlatform == TargetPlatform.android &&
+        widget.useHybridComposition) {
+      HybridAndroidViewController.checkIfSupported().then((supported) {
+        if (mounted) {
+          setState(() => _hcppSupported = supported);
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     if (defaultTargetPlatform != TargetPlatform.android &&
         defaultTargetPlatform != TargetPlatform.iOS) {
-      return Container();
+      return const SizedBox.shrink();
     }
-    final UniqueKey _key = UniqueKey();
-    final viewType = 'suamusica/pre_roll_view';
-    final creationParams = <String, dynamic>{
-      'adSize': {
-        'width': 640,
-        'height': 480,
-      },
-    };
 
     debugPrint('[PREROLL] Building PreRoll');
 
     return Container(
       constraints: BoxConstraints(
-        maxHeight: maxHeight ?? MediaQuery.of(context).size.height,
+        maxHeight: widget.maxHeight ?? MediaQuery.of(context).size.height,
       ),
-      child: defaultTargetPlatform == TargetPlatform.android
-          ? (useHybridComposition
-              ? PlatformViewLink(
-                  key: _key,
-                  viewType: viewType,
-                  surfaceFactory: (BuildContext context,
-                          PlatformViewController controller) =>
-                      AndroidViewSurface(
-                    controller: controller as AndroidViewController,
-                    gestureRecognizers: const <Factory<
-                        OneSequenceGestureRecognizer>>{},
-                    hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-                  ),
-                  onCreatePlatformView: (PlatformViewCreationParams params) =>
-                      (useinitExpensiveAndroidView
-                          ? PlatformViewsService.initExpensiveAndroidView(
-                              id: params.id,
-                              viewType: viewType,
-                              layoutDirection: TextDirection.ltr,
-                              creationParams: creationParams,
-                              creationParamsCodec: const StandardMessageCodec(),
-                            )
-                          : PlatformViewsService.initSurfaceAndroidView(
-                              id: params.id,
-                              viewType: viewType,
-                              layoutDirection: TextDirection.ltr,
-                              creationParams: creationParams,
-                              creationParamsCodec: const StandardMessageCodec(),
-                            ))
-                        ..addOnPlatformViewCreatedListener((int id) {
-                          params.onPlatformViewCreated(id);
-                          _onPlatformViewCreated(id);
-                        })
-                        ..create(),
-                )
-              : AndroidView(
-                  key: _key,
-                  viewType: viewType,
-                  creationParams: creationParams,
-                  creationParamsCodec: const StandardMessageCodec(),
-                  onPlatformViewCreated: _onPlatformViewCreated,
-                  clipBehavior: Clip.none,
-                ))
-          : UiKitView(
-              key: _key,
-              viewType: viewType,
-              creationParams: creationParams,
-              creationParamsCodec: const StandardMessageCodec(),
-              onPlatformViewCreated: _onPlatformViewCreated,
-            ),
+      child: switch (defaultTargetPlatform) {
+        TargetPlatform.android => _PreRollAndroidView(
+            useHybridComposition: widget.useHybridComposition,
+            useinitExpensiveAndroidView: widget.useinitExpensiveAndroidView,
+            hcppSupported: _hcppSupported,
+            controller: widget.controller,
+            platformViewKey: _platformViewKey,
+          ),
+        _ => _PreRollIosView(
+            controller: widget.controller,
+            platformViewKey: _platformViewKey,
+          ),
+      },
     );
   }
+}
 
-  void _onPlatformViewCreated(int id) {
-    debugPrint('Platform view created withs id: $id');
-    if (Platform.isAndroid) {
-      controller?.play();
+class _PreRollAndroidView extends StatelessWidget {
+  const _PreRollAndroidView({
+    required this.useHybridComposition,
+    required this.useinitExpensiveAndroidView,
+    required this.hcppSupported,
+    required this.controller,
+    required this.platformViewKey,
+  });
+
+  final bool useHybridComposition;
+  final bool useinitExpensiveAndroidView;
+  final bool? hcppSupported;
+  final PreRollController? controller;
+  final Key platformViewKey;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!useHybridComposition) {
+      return _PreRollTextureLayerAndroidView(
+        controller: controller,
+        platformViewKey: platformViewKey,
+      );
     }
+
+    if (hcppSupported == null) {
+      return const SizedBox.shrink();
+    }
+
+    return _PreRollHybridPlatformViewLink(
+      useHcpp: hcppSupported!,
+      useinitExpensiveAndroidView: useinitExpensiveAndroidView,
+      controller: controller,
+      platformViewKey: platformViewKey,
+    );
+  }
+}
+
+class _PreRollHybridPlatformViewLink extends StatelessWidget {
+  const _PreRollHybridPlatformViewLink({
+    required this.useHcpp,
+    required this.useinitExpensiveAndroidView,
+    required this.controller,
+    required this.platformViewKey,
+  });
+
+  final bool useHcpp;
+  final bool useinitExpensiveAndroidView;
+  final PreRollController? controller;
+  final Key platformViewKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformViewLink(
+      key: platformViewKey,
+      viewType: _kPreRollViewType,
+      surfaceFactory: (
+        BuildContext context,
+        PlatformViewController controller,
+      ) =>
+          AndroidViewSurface(
+        controller: controller as AndroidViewController,
+        gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+        hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+      ),
+      onCreatePlatformView: (PlatformViewCreationParams params) {
+        final AndroidViewController viewController = useHcpp
+            ? PlatformViewsService.initHybridAndroidView(
+                id: params.id,
+                viewType: _kPreRollViewType,
+                layoutDirection: TextDirection.ltr,
+                creationParams: _kPreRollCreationParams,
+                creationParamsCodec: _kPreRollCreationParamsCodec,
+                onFocus: () => params.onFocusChanged(true),
+              )
+            : useinitExpensiveAndroidView
+                ? PlatformViewsService.initExpensiveAndroidView(
+                    id: params.id,
+                    viewType: _kPreRollViewType,
+                    layoutDirection: TextDirection.ltr,
+                    creationParams: _kPreRollCreationParams,
+                    creationParamsCodec: _kPreRollCreationParamsCodec,
+                  )
+                : PlatformViewsService.initSurfaceAndroidView(
+                    id: params.id,
+                    viewType: _kPreRollViewType,
+                    layoutDirection: TextDirection.ltr,
+                    creationParams: _kPreRollCreationParams,
+                    creationParamsCodec: _kPreRollCreationParamsCodec,
+                  );
+
+        return viewController
+          ..addOnPlatformViewCreatedListener((int id) {
+            params.onPlatformViewCreated(id);
+            _onPreRollPlatformViewCreated(controller, id);
+          })
+          ..create();
+      },
+    );
+  }
+}
+
+class _PreRollTextureLayerAndroidView extends StatelessWidget {
+  const _PreRollTextureLayerAndroidView({
+    required this.controller,
+    required this.platformViewKey,
+  });
+
+  final PreRollController? controller;
+  final Key platformViewKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return AndroidView(
+      key: platformViewKey,
+      viewType: _kPreRollViewType,
+      creationParams: _kPreRollCreationParams,
+      creationParamsCodec: _kPreRollCreationParamsCodec,
+      onPlatformViewCreated: (id) => _onPreRollPlatformViewCreated(controller, id),
+      clipBehavior: Clip.none,
+    );
+  }
+}
+
+class _PreRollIosView extends StatelessWidget {
+  const _PreRollIosView({
+    required this.controller,
+    required this.platformViewKey,
+  });
+
+  final PreRollController? controller;
+  final Key platformViewKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return UiKitView(
+      key: platformViewKey,
+      viewType: _kPreRollViewType,
+      creationParams: _kPreRollCreationParams,
+      creationParamsCodec: _kPreRollCreationParamsCodec,
+      onPlatformViewCreated: (id) => _onPreRollPlatformViewCreated(controller, id),
+    );
+  }
+}
+
+void _onPreRollPlatformViewCreated(PreRollController? controller, int id) {
+  debugPrint('Platform view created withs id: $id');
+  if (Platform.isAndroid) {
+    controller?.play();
   }
 }
